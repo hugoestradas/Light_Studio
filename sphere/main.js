0c2d90bda53640050cb3aeaf9925e771bebb0d75
hugoestradas <hugo_estradas@outlook.com>
2021-03-15 16:45:26 -0600

    modified light in sphere project

--- sphere/main.js
+++ sphere/main.js
@@ -79,7 +79,7 @@ function init() {
 
 	var camera = new THREE.PerspectiveCamera(
 		45, 
-		window.innerWidth / window.innerHeight, // aspect ratio
+		window.innerWidth / window.innerHeight, 
 		1, 
 		1000 
 	);
